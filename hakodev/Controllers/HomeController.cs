502f3a7f5403186dfdbbfa9a728a8fa5372b4348
Ha Dinh <ha.dinh2@niteco.se>
2025-08-25 13:34:19 +0700

    Add project files.

---
+++ hakodev/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace hakodev.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World! This is a .NET 9.0 application running on Windows.");
+        }
+
+        [HttpGet("health")]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Service is healthy!");
+        }
+
+        [HttpGet("version")]
+        public IActionResult Version()
+        {
+            return Ok(new { Version = "1.0.0", Framework = "net9.0" });
+        }
+    }
+}
