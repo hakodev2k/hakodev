edbdc7520bf57e725766c364c8d9a27a3d26a215
hakodev2k <ha5210032@lms.utc.edu.vn>
2025-09-12 11:05:48 +0700

    Update version to 1.0.7 in HomeController

--- hakodev/Controllers/HomeController.cs
+++ hakodev/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace hakodev.Controllers
         [HttpGet("version")]
         public IActionResult Version()
         {
-            return Ok(new { Version = "1.0.6", Framework = "net8.0" });
+            return Ok(new { Version = "1.0.7", Framework = "net8.0" });
         }
     }
 }
